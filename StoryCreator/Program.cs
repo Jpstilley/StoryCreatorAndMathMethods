433128424db7d628d9fc051bea43aa4282334a2a
Joey Stilley <Joey.Stilley@gmail.com>
2021-08-23 15:19:56 -0500

    MathMethods Branch

--- StoryCreator/Program.cs
+++ StoryCreator/Program.cs
@@ -24,8 +24,6 @@ namespace StoryCreator
 
             Divide(10000, 2, 3, 2, wildCard);
             Console.WriteLine(quotient);
-
-            Console.WriteLine(CreateStory());
         }
 
         public static void Adding(params int[] parameters)
@@ -69,60 +67,5 @@ namespace StoryCreator
             }
             quotient = result;
         }
-
-        public static string CreateStory()
-        {
-            Console.WriteLine("What is your name?");
-
-            string name = Console.ReadLine();
-
-            Console.WriteLine("What is a silly word?");
-
-            string sillyWord = Console.ReadLine();
-
-            Console.WriteLine("Enter a number.");
-
-            string number = Console.ReadLine().ToLower();
-
-            Console.WriteLine("Enter an adjective.");
-
-            string adjective1 = Console.ReadLine().ToLower();
-
-            Console.WriteLine("Enter a noun.");
-
-            string noun = Console.ReadLine();
-
-            Console.WriteLine("Enter another adjective.");
-
-            string adjective2 = Console.ReadLine();
-
-            Console.WriteLine("Enter a family relative type.");
-
-            string relative = Console.ReadLine();
-
-            Console.WriteLine("Enter another adjective.");
-
-            string adjective3 = Console.ReadLine();
-
-            Console.WriteLine("Enter a verb.");
-
-            string verb = Console.ReadLine();
-
-            Console.WriteLine("Enter another adjective.");
-
-            string adjective4 = Console.ReadLine();
-
-            Console.WriteLine("Enter another adjective.");
-
-            string adjective5 = Console.ReadLine();
-
-            string madLib = $"Hello, my name is {name} I am on my way to the planet {sillyWord}.\nI will be gone for {number} days." +
-                                 $"\nI am very {adjective1} about the trip but I will miss my {noun}.\nI have heard the atmosphere their is " +
-                                 $"{adjective2}.\nLuckily my {relative} packed me a jacket to keep me {adjective3}.\nWhen I land of the planet, I will {verb} for joy." +
-                                 $"\nI am {adjective4} to walk on another planet.\nI could not be more {adjective5} for this trip!";
-
-            return madLib;
-        }
-
     }
 }
